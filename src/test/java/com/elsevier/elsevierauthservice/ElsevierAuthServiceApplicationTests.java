42df3faa0fd37ed85e595de981690ffd042a4050
An Phuc Thien Nguyen <npta97@gmail.com>
2020-07-29 17:53:50 +0700

    Init project

---
+++ src/test/java/com/elsevier/elsevierauthservice/ElsevierAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsevier.elsevierauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsevierAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
